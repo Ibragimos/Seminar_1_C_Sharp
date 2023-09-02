c47be93699f8203e25e13aa1a648e97ac3fc7854
Marina <korhovama@mail.ru>
2023-09-03 00:40:14 +0300

    Convert changed to Parse

--- Program.cs
+++ Program.cs
@@ -1,7 +1,7 @@
 ﻿System.Console.Write("Enter a number 1: ");
- int num1 = Convert.ToInt32(Console.ReadLine());
+ int num1 = Int32.Parse(Console.ReadLine());
  System.Console.Write("Enter a number 2: ");
- int num2 = Convert.ToInt32(Console.ReadLine());
+ int num2 = Int32.Parse(Console.ReadLine());
  
  if(num1 > num2) System.Console.Write($"{num1} > {num2}");
  else if(num1 < num2) System.Console.Write($"{num2} > {num1}");
