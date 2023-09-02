d731ac2de00e4f0cbad7879f2b69f4e83b0a9ec8
Marina <korhovama@mail.ru>
2023-09-03 00:34:07 +0300

    Add code

--- Program.cs
+++ Program.cs
@@ -1,1 +1,5 @@
-﻿
+﻿Console.WriteLine("Enter a number:");
+int num = Convert.ToInt32(Console.ReadLine());
+
+if(num % 2 == 0) Console.WriteLine("Even number");
+else Console.WriteLine("Odd number");
