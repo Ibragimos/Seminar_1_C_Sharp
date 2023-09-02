6b0f81f10b4e860599323559fcffb97a0b4c222a
Marina <korhovama@mail.ru>
2023-09-03 00:06:29 +0300

    Add code

--- Program.cs
+++ Program.cs
@@ -1,2 +1,17 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿System.Console.Write("Enter a number 1: ");
+ int num1 = Convert.ToInt32(Console.ReadLine());
+ System.Console.Write("Enter a number 2: ");
+ int num2 = Convert.ToInt32(Console.ReadLine());
+ 
+ if(num1 > num2)
+ {
+    System.Console.Write($"{num1} > {num2}");
+ }
+ else if(num1 < num2)
+ {
+    System.Console.Write($"{num2} > {num1}");
+ }
+ else
+ {
+    System.Console.Write($"{num1} = {num2}");
+ }
